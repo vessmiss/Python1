d5ea24a423291ba75d3f8635599b5f76cccbc843
vessmiss <125880612+vessmiss@users.noreply.github.com>
2023-02-27 15:48:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+
+a =int( input("Введите значение a:"))
+print(a)
+z1 = 2 * math.pow(math.sin(3 * math.pi - 2 * a),2)5 * math.pow(math.cos(5 * math.pi + 2 * a),2)
+print(z1)
+
+a =int( input("Введите значение a:"))
+print(a)
+z2 = 1 / 4 - 1 / 4 * math.sin(5 / 2 * math.pi - 8 * a)
+print(z2)
+
